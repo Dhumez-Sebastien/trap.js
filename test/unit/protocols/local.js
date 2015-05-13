4c1a49c3b8f13aa72987d984f0688a9e2d6b9cf0
Seb <sebastien.dhumez@gmail.com>
2015-05-13 20:06:00 +0200

    Add simple test for travis

--- test/unit/protocols/local.js
+++ test/unit/protocols/local.js
@@ -3,12 +3,11 @@
 describe('Unit :: Protocols.Local', function () {
 
     var Local = require('../../../src/protocols/list/Local.js');
+    var local = new Local();
 
-    it('constructor()', function() {
-
-        var local = new Local();
-
+    it('Test methods', function() {
         assert(typeof local == 'object', 'No found protocol.local : '+local);
         assert(typeof local.getName == 'function', 'protocol.local.getName function error : '+local.getName);
+        assert(typeof local._accountIsLocked == 'function', 'protocol.local._accountIsLocked function error : '+local._accountIsLocked);
     });
 });
